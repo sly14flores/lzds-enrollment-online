a1dfcd8267bdd8edf0b48a8b5e8c682175d64ad4
Jp <johnpaulbalanon6@gmail.com>
2021-05-06 10:30:31 +0800

    Topbar

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="layout-topbar">
-        <img src="/images/lzds-logo-small.png" class="logo" />
-        <span style="color: #000; margin-left: 100px; font-size: 1.3rem">Online Enrollment System</span>
-        <div class="layout-topbar-icons" style="color: #000; font-size: 1.2rem">
-            SY: 2021-22
+        
+        <div class="p-grid">
+            <div class="p-col-9 p-md-9">
+                <img src="/images/lzds-logo-small.png" class="logo" />
+                <p class="p-text-size">Online Enrollment System</p>
+            </div>
+            <div class="p-col-3 p-md-3">
+                <p class="sy">SY: 2021-22</p>
+            </div>
         </div>
+        
     </div>
 </template>
 
@@ -14,5 +20,27 @@
         position: absolute;
         top: 0;
         margin-top: 1px;
+    }
+    .p-text-size {
+        color: #000; 
+        margin-left: 100px; 
+        font-size: 1.5rem
+    }
+    .sy{
+        color: #000;
+        font-size: 1.5rem;
+        text-align: right;
+    }
+    @media only screen and (max-width: 600px) {
+        .p-text-size {
+            color: #000; 
+            margin-left: 100px; 
+            font-size: 13px
+        }
+        .sy {
+            color: #000;
+            font-size: 13px;
+            text-align: right;
+        }
     }
 </style>
